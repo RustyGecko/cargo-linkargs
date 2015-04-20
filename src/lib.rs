4a270535155f758e69d5c372f414f3fd0ad2837a
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-04-21 00:20:05 +0200

    Test appending of linkargs

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,11 @@
-#![allow(warnings)]
+#![deny(warnings)]
 
 extern crate cargo;
 
-use std::process::{Output, Command, Stdio};
-use std::path::{Path, PathBuf};
-use std::fs::File;
-use std::io::{self, BufReader, BufRead, BufWriter, Write, copy};
-// use std::os::self_exe_path;
-use std::mem::swap;
+use std::process::Output;
 
-use cargo::ops::{ExecEngine, CommandPrototype, CommandType};
-use cargo::util::{self, ProcessError, ProcessBuilder};
-use cargo::core::PackageIdSpec;
+use cargo::ops::{ExecEngine, CommandPrototype};
+use cargo::util::{ProcessError, ProcessBuilder};
 
 type ExecResult = Result<Option<Output>, ProcessError>;
 
@@ -24,17 +18,21 @@ pub struct LinkArgsEngine {
 
 impl ExecEngine for LinkArgsEngine {
     fn exec(&self, command: CommandPrototype) -> Result<(), ProcessError> {
-        append_linkargs(command, false, self).map(|_| ())
+        exec_append_linkargs(command, false, self).map(|_| ())
     }
 
     fn exec_with_output(&self, command: CommandPrototype) -> Result<Output, ProcessError> {
-        append_linkargs(command, true, self).map(|a| a.unwrap())
+        exec_append_linkargs(command, true, self).map(|a| a.unwrap())
     }
 }
 
-fn append_linkargs(mut command: CommandPrototype, with_output: bool,
-                   engine: &LinkArgsEngine) -> ExecResult {
+fn exec_append_linkargs(command: CommandPrototype, with_output: bool,
+                        engine: &LinkArgsEngine) -> ExecResult {
+    let command = append_linkargs(command, engine);
+    execute(command.into_process_builder(), with_output)
+}
 
+fn append_linkargs(mut command: CommandPrototype, engine: &LinkArgsEngine) -> CommandPrototype {
     let name_matches = command.get_args().windows(2).find(|&args| {
         args[0].to_str() == Some("--crate-name") &&
         args[1].to_str() == Some(&engine.pkg_name)
@@ -55,7 +53,7 @@ fn append_linkargs(mut command: CommandPrototype, with_output: bool,
         }
     }
 
-    execute(command.into_process_builder(), with_output)
+    command
 }
 
 fn execute(process: ProcessBuilder, with_output: bool) -> ExecResult {
@@ -63,5 +61,40 @@ fn execute(process: ProcessBuilder, with_output: bool) -> ExecResult {
         process.exec_with_output().map(|o| Some(o))
     } else {
         process.exec().map(|_| None)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use cargo::ops::{CommandPrototype, CommandType};
+    use {append_linkargs, LinkArgsEngine};
+
+    fn cmd_and_engine(name: &str) -> (CommandPrototype, LinkArgsEngine) {
+        let mut cmd = CommandPrototype::new(CommandType::Rustc).unwrap();
+        cmd.args(&["--crate-name", "test_bin", "--crate-type", "bin"]);
+        let engine = LinkArgsEngine {
+            pkg_name: name.to_string(),
+            link_args: Some("-first --num 2".to_string()),
+            print_link_args: true,
+        };
+        (cmd, engine)
+    }
+
+    #[test]
+    fn do_append_linkargs() {
+        let (cmd, engine) = cmd_and_engine("test_bin");
+
+        let cmd = append_linkargs(cmd, &engine);
+        assert!(cmd.get_args().contains(&From::from("link-args=-first --num 2")));
+        assert!(cmd.get_args().contains(&From::from("print-link-args")))
+    }
+
+    #[test]
+    fn not_append_linkargs() {
+        let (cmd, engine) = cmd_and_engine("test_lib");
+
+        let cmd = append_linkargs(cmd, &engine);
+        assert!(!cmd.get_args().contains(&From::from("link-args=-first --num 2")));
+        assert!(!cmd.get_args().contains(&From::from("print-link-args")))
     }
 }
