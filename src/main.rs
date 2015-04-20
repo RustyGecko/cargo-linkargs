f0563fdb46583d5cb9e69fb3a705ab0a8d85901a
Sondre Lefsaker <sondrele@stud.ntnu.no>
2015-04-20 23:17:38 +0200

    Use docopt macro to decode arguments, and remove link-args argument in favor of a normal argument

--- src/main.rs
+++ src/main.rs
@@ -19,25 +19,11 @@ use cargo::core::{MultiShell, PackageIdSpec};
 
 use cargo_linkargs::LinkArgsEngine;
 
-#[derive(RustcDecodable, Debug)]
-struct Options {
-    flag_package: Option<String>,
-    flag_jobs: Option<u32>,
-    flag_features: Vec<String>,
-    flag_no_default_features: bool,
-    flag_target: Option<String>,
-    flag_manifest_path: Option<String>,
-    flag_link_args: Option<String>,
-    flag_verbose: bool,
-    flag_release: bool,
-    flag_lib: bool
-}
-
-pub const USAGE: &'static str = "
+docopt!(Options derive Debug, "
 Compile a local package and all of its dependencies
 
 Usage:
-    cargo-linkargs [options]
+    cargo linkargs [options] <args>
 
 Options:
     -h, --help               Print this message
@@ -49,7 +35,6 @@ Options:
     --no-default-features    Do not build the `default` feature
     --target TRIPLE          Build for the target triple
     --manifest-path PATH     Path to the manifest to compile
-    --link-args ARGS         A string with extra arguments to the linker
     -v, --verbose            Use verbose output
 
 If the --package argument is given, then SPEC is a package id specification
@@ -60,7 +45,14 @@ current package is built. For more information on SPEC and its format, see the
 Compilation can be configured via the use of profiles which are configured in
 the manifest. The default profile for this command is `dev`, but passing
 the --release flag will use the `release` profile instead.
-";
+",
+arg_args: Option<String>,
+flag_package: Option<String>,
+flag_jobs: Option<u32>,
+flag_features: Vec<String>,
+flag_target: Option<String>,
+flag_manifest_path: Option<String>,
+);
 
 fn get_package_name(root: &PathBuf, spec: Option<&str>, shell: &mut MultiShell) -> Option<String> {
     let pkg_name = {
@@ -82,9 +74,9 @@ fn get_package_name(root: &PathBuf, spec: Option<&str>, shell: &mut MultiShell) 
 }
 
 fn main() {
-    let options: Options = Docopt::new(USAGE)
-                                   .and_then(|d| d.decode())
-                                   .unwrap_or_else(|e| e.exit());
+    let options: Options = Options::docopt()
+                                    .decode()
+                                    .unwrap_or_else(|e| e.exit());
     let mut shell = cargo::shell(options.flag_verbose);
     let root = find_root_manifest_for_cwd(options.flag_manifest_path).unwrap();
     let spec = options.flag_package.as_ref().map(|s| &s[..]);
@@ -103,7 +95,7 @@ fn main() {
 
         let engine = LinkArgsEngine {
             pkg_name: pkg_name,
-            link_args: options.flag_link_args.clone(),
+            link_args: options.arg_args.clone(),
         };
 
         let mut opts = CompileOptions {
